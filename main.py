5b16303e9b1b02108d93b51cb7d8a5dcdfa1691e
Roman Gromov <gromov1921@bk.ru>
2023-07-29 15:38:50 +0300

    last version

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import logging
 import os
 import time
-import logging
-import sys
+from datetime import datetime
 
 from dotenv import load_dotenv
 from telegram import Bot
-from datetime import datetime
 
+from messages import (
+    CHECK_TOKENS_START_TEXT, MISSING_TOKENS_TEXT, CHECK_TOKENS_END_TEXT
+)
 from order_parser import Parser
-from messages import *
 
 CURRENT_DATE = datetime.now().__str__().replace(
     ' ', '-'
@@ -26,8 +27,6 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('token')
 
-CHAT_ID = os.getenv('chat_id')
-
 CMS_LINK = os.getenv('cms_link')
 
 CMS_LOGIN = os.getenv('cms_login')
@@ -40,7 +39,6 @@ def check_tokens():
     logging.debug(CHECK_TOKENS_START_TEXT)
     for name, value in (
         ('tg_token', TELEGRAM_TOKEN),
-        ('chat_id', CHAT_ID),
         ('cms_link', CMS_LINK),
         ('cms_login', CMS_LOGIN),
         ('cms_password', CMS_PASSWORD)
@@ -53,28 +51,32 @@ def check_tokens():
 
 if __name__ == '__main__':
     check_tokens()
+    try:
+        bot = Bot(TELEGRAM_TOKEN)
+        parser = Parser(
+            link=CMS_LINK,
+            email=CMS_LOGIN,
+            password=CMS_PASSWORD
+        )
 
-    bot = Bot(TELEGRAM_TOKEN)
-    parser = Parser(
-        link=CMS_LINK,
-        email=CMS_LOGIN,
-        password=CMS_PASSWORD
-    )
-    
-    parser.configure_parser()
-    order = parser.start_parsing()
-
-    while True:
-        try:
-            parser.driver.refresh()
-            if order is not None:
-                bot.send_message(
-                    chat_id=CHAT_ID,
-                    text=order.__str__()
-                )
-        except Exception as error:
-            print('Возникла ошибка: {}'.format(error))
-        finally:
-            print('#############')
-            parser.return_to_homepage()
-            time.sleep(10)
+        parser.configure_parser()
+        parser.authenticate_parser()
+    except Exception as error:
+        print('Возникла ошибка: {}'.format(error))
+    else:
+        while True:
+            try:
+                parser.driver.refresh()
+                order = parser.handle_order_info()
+                print(order)
+                if order is not None:
+                    for chat_id in (539288377, 1258274970, 6192766051):
+                        bot.send_message(
+                            chat_id=chat_id,
+                            text=order.__str__()
+                        )
+            except Exception as error:
+                logging.debug('Возникла ошибка: {}'.format(error))
+            finally:
+                parser.return_to_homepage()
+                time.sleep(10)
